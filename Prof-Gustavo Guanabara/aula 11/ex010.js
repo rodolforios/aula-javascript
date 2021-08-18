2b6a92a2f58f0a8d166f377cbbc9ecde8c632b40
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-08-18 11:36:50 -0300

    aula 11

---
+++ Prof-Gustavo Guanabara/aula 11/ex010.js
@@ -0,0 +1,17 @@
+/*
+var vel = 55.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel>60){
+    console.log('Você ultrapassou a velocidade permitida.Multado!')
+}else{
+    console.log('Dirija sempre usando cinto de segurança!')
+}
+*/
+
+var pais ='EUA'
+if(pais =='Brasil'){
+    console.log('Brasileiro!')
+}else{
+    console.log('Estrangeiro!')
+}
