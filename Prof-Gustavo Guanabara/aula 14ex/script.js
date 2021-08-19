9fcea17a13202aba6289f7cf955079368fbfc6f2
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-08-19 12:19:07 -0300

    inicio da aula 14

---
+++ Prof-Gustavo Guanabara/aula 14ex/script.js
@@ -0,0 +1,11 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear() 
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if(fano.nodeValue.length==0 || fano.value>ano){
+        window.alert('Verifique os dados e tente novamente!')
+    }else{
+        window.alert('Tudo ok!')
+    }
+}
