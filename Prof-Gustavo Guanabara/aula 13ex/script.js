d69a78d7d0c627cc10c689dce64bc3aae76050eb
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-08-19 11:35:07 -0300

    aula 13

---
+++ Prof-Gustavo Guanabara/aula 13ex/script.js
@@ -0,0 +1,30 @@
+
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+var minutos= data.getMinutes()
+
+msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
+
+
+
+if(hora >=0 && hora < 12){
+    //bom dia!
+img.src='img/manha.png'
+document.body.style.background='#e2cd9f'
+
+}else if(hora >= 12 && hora <= 18){
+img.src='img/tarde.png'
+document.body.style.background='#b9846f'
+
+//boa tarde
+
+}else{
+    //boa noite
+    img.src='img/noite.png'
+    document.body.style.background='#515154'
+}
+
+}
