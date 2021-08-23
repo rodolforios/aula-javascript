c277aef5382ba3b69d8b9302608bc0cf2549df80
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-08-23 12:52:09 -0300

    exericio tabuada concluído

--- Prof-Gustavo Guanabara/aula 16ex/ex002/script.js
+++ Prof-Gustavo Guanabara/aula 16ex/ex002/script.js
@@ -1,23 +1,65 @@
 function calcular() {
-    var num = document.getElementById('txtnum')
-    var tab = Number(num.value)
-    var res = document.querySelector('div#exib')
-    res.innerHTML =''
 
-    if( num.value.length ==0){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
         window.alert('Digite um número')
-    }else{
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML=''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value=` ${c}`
+            tab.appendChild(item)
+            c++
 
-    for (i = 0; i <= 10; i++) {
-        res.innerHTML += `${tab} x ${i} = ${tab * i} <br>` 
-        
+
+        }
     }
 
 
-
-
-    
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function calcular() {
+    var num = document.getElementById('txtnum')
+    var tab = Number(num.value)
+    var res = document.querySelector('div#exib')
+    res.innerHTML = ''
+
+    if (num.value.length == 0) {
+        window.alert('Digite um número')
+    } else {
+        res.innerHTML = ''
+        for (i = 0; i <= 10; i++) {
+            res.innerHTML += `${tab} x ${i} = ${tab * i} <br>`
+
+        }
+
+
+
+
+
+
+
+    }
 }
+
+*/
