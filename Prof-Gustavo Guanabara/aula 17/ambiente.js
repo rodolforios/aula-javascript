a6ffcef4e455e17f0040490b5832d7bfc5a21d63
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-08-25 05:29:19 -0300

    aula 16

---
+++ Prof-Gustavo Guanabara/aula 17/ambiente.js
@@ -0,0 +1,35 @@
+
+
+let num = [4,6,4,6,8,7]
+
+
+console.log(`O vetor tem ${num.length} posições` )
+console.log(`O vetor em ordem crescente é ${num.sort()}`)
+
+let pos=num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+
+
+/*
+
+num[6] =12 // Para acrescentar um elemento
+num.push(7) // Para acrescentar um elemento
+
+num.length//comprimento do array
+
+num.sort() //coloca todos os números em ordem crescente
+*/
+
+/*
+
+//mostrar todos os números divisiveis por 7
+var a = []
+
+for(i=1;i<= 100;i++){
+    if( i%7==0){
+        a.push(i)
+
+    }
+}
+
+*/
---
+++ Prof-Gustavo Guanabara/vetorNaTela.js
@@ -0,0 +1,32 @@
+/*
+Imprimir os valores do array
+let valores =[8,1,7,4,2,9]
+
+for( i = 0; i< valores.length;i++){
+    console.log(`A Posição ${i} tem o valor ${valores[i]}`)
+}
+
+*/
+//https://ricardo-reis.medium.com/o-m%C3%A9todo-sort-do-array-javascript-482576734e0a
+
+//numeros em ordem crescente
+/*
+let numbers = [0,1,2,3,10,20,30]
+numbers.sort(function(a,b)   {
+    if(a > b) return 1;
+    if(a < b) return -1;
+
+    return 0;
+});
+console.log(numbers);
+*/
+
+
+
+let valores =[8,1,7,4,2,9]
+/*
+valores.sort()
+for(let pos in valores){
+    console.log(`A Posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
