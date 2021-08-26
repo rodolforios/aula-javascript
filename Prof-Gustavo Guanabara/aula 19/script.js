71a12668201fcbabd4b2896d70a25425a854196e
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-08-26 11:36:15 -0300

    aula 17

--- Prof-Gustavo Guanabara/aula 19/script.js
+++ Prof-Gustavo Guanabara/aula 19/script.js
@@ -1,56 +1,65 @@
-function calcular() {
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
-        window.alert('Digite um número')
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        let n = Number(num.value)
-        let c = 1
-        tab.innerHTML=''
-        while (c <= 10) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n * c}`
-            item.value=` ${c}`
-            tab.appendChild(item)
-            c++
-
-
-        }
+        return false
     }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+
+
 
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function calcular() {
-    var num = document.getElementById('txtnum')
-    var tab = Number(num.value)
-    var res = document.querySelector('div#exib')
-    res.innerHTML = ''
-
-    if (num.value.length == 0) {
-        window.alert('Digite um número')
-    } else {
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
         res.innerHTML = ''
-        for (i = 0; i <= 10; i++) {
-            res.innerHTML += `${tab} x ${i} = ${tab * i} <br>`
 
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+
+
+
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione Valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+
+        for (let pos in valores) {
+            soma+=valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
         }
 
 
@@ -58,8 +67,14 @@ function calcular() {
 
 
 
-
+        res.innerHTML = `  `
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>  `
+        res.innerHTML += `<p>O maior valor informado foi  ${maior}.</p>  `
+        res.innerHTML += `<p>O menor valor informado foi  ${menor}.</p>  `
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>  `
+        res.innerHTML += `<p>A média de todos os valores é ${soma/tot.toFixed(2)}.</p>  `
     }
+
+
 }
 
-*/
