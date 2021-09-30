e6a121b136334a2af1cbe5eae4f843818d1fb5ab
Rodolfo Rios <rodolforios18@gmail.com>
2021-09-30 12:30:40 -0300

    .

---
+++ Prof-Luis Otavio/Seção 3/12-exercicio_switch_date/assets/js/main.js
@@ -0,0 +1,24 @@
+
+
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+const opcoes ={
+    dateStyle:'full',
+    timeStyle:'short'
+
+}
+//h1.innerHTML = data.toLocaleDateString('pt-BR',opcoes)
+
+h1.innerHTML = Intl.DateTimeFormat('pt-BR', { dateStyle: "full", timeStyle: "short" }).format(new Date())
+
+
+
+
+
+
+
+
+
+
+
+
