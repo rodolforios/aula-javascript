cefbd831ea4bc6c9394c3c391c6064978d0735be
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-10-07 19:41:17 -0300

    Create maior_numero.js

---
+++ Prof-Luis Otavio/Seção 3/19-Exercicios/maior_numero.js
@@ -0,0 +1,20 @@
+//Escreva uma  função que receba 2 números e retorne o maior deles.
+
+function max1(a,b){
+   
+   
+   return a>b?a:b; // Função ternaria
+   
+      
+}
+
+
+const max2 = (x,y)=> x > y ? x : y; //Arrow function.
+
+
+
+
+console.log(max1(5,1))
+
+console.log(max2(55,12))
+
