63a1123ed4497d873c997d13174bf5f5bd49fd9f
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2021-10-07 21:07:24 -0300

    Update aula_2.js

--- Prof-Luis Otavio/Seção 3/20-try,catch,throw/aula_2.js
+++ Prof-Luis Otavio/Seção 3/20-try,catch,throw/aula_2.js
@@ -1,3 +1,46 @@
+
+/*
 try{
-    //executa
+    //Executa quando não há erros
+    
+    console.log('Abrir um arquivo');
+    console.log('Manipulei o arquivo e gerou erro');
+    console.log(a);
+    console.log('Fechei o arquivo');
+
+}catch(err){
+
+    //Executa quando não há erros
+
+    console.log('Tratando o erro')
+
+
+} finally{
+    //É executado sempre
+
+    console.log('Eu sempre sou executado')
+
+
 }
+
+*/
+
+function retornaHora(data){
+
+if(data && !(data instanceof Date)){
+    throw new TypeError('Esperando instância de Date.')
+    
+}
+if (!data){
+    data = new Date();
+}
+
+return data.toLocaleTimeString('pt-BR',{
+hour12:false
+});
+}
+
+const hora = retornaHora();
+console.log(hora);
+
+
