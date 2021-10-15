e80b1e0abb0c2a3954945889076e5b1f85ee6556
Rodolfo Rios <rodolforios18@gmail.com>
2021-10-15 11:06:06 -0300

    aula 77

--- Prof-Luis Otavio/seção 4/10-funcoes_construtoras/index.js
+++ Prof-Luis Otavio/seção 4/10-funcoes_construtoras/index.js
@@ -1,2 +1,27 @@
 //Funcao construtoras - objetos.
 
+//Construtora sempre começa com letra maiuscula
+// é necessário instanciar
+
+function Pessoa(nome,sobrenome){
+    //Atributos ou metodos privados não usam 'this'
+
+    const ID = 123456;
+    const metodoInterno = function(){
+    
+}
+
+
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodo = function(){
+        console.log(this.nome + ': sou um metodo');
+    }
+}
+
+const p1 = new Pessoa('Rodolfo','Rios');
+const p2 = new Pessoa('Felipe', 'Oliveira');
+p1.metodo();
+
+console.log(p2.nome);
