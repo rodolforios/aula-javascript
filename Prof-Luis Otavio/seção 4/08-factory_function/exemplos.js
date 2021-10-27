646d5fd0ec745ab86250cf56e28e9125f1245e71
Rodolfo Rios <rodolforios18@gmail.com>
2021-10-27 08:57:20 -0300

    revisao de funcoes

---
+++ Prof-Luis Otavio/seção 4/08-factory_function/exemplos.js
@@ -0,0 +1,5 @@
+//A factory function serve para criar objetos, você cria uma função
+//com todos atributos e metodos e depois criar objetos.
+
+//a factory is a function that returns an object, while a class is a template for an object.
+
--- Prof-Luis Otavio/seção 4/08-factory_function/factory.js
+++ Prof-Luis Otavio/seção 4/08-factory_function/factory.js
@@ -1,5 +1,11 @@
 // Factory function (Função fábrica)
-// Constructor function (Função construtora)
+// Constructor function (Função construtora).
+
+//A factory function serve para criar objetos, você cria uma função
+//com todos atributos e metodos e depois criar objetos.
+
+//a factory is a function that returns an object, while a class is a template for an object.
+
 function criaPessoa(nome, sobrenome, a, p) {
   return {
     nome,
