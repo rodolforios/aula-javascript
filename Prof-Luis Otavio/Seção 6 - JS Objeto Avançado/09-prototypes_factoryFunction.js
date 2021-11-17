0a1d87d07997d3555643c05f9eca5fb82eefefd4
Rodolfo Rios <rodolforios18@gmail.com>
2021-11-17 09:09:34 -0300

    aula 100

--- Prof-Luis Otavio/Seção 6 - JS Objeto Avançado/09-prototypes_factoryFunction.js
+++ Prof-Luis Otavio/Seção 6 - JS Objeto Avançado/09-prototypes_factoryFunction.js
@@ -1,23 +1,30 @@
-function criaèssoa(nome,sobrenome){
-
-    const pessoaPrototype = {
-        falar(){
-            console.log(`${this.nome} está falando.`)
-        },
-        comer(){
-            console.log(`${this.nome} está comendo.`)
-        },
-        beber(){
-            console.log(`${this.nome} está bebendo.`)
-        }
-
-    }
-
-    return{
-        nome,
-        sobrenome,
-        
-    };
-};
-
-
+const falar = {
+    falar() {
+      console.log(`${this.nome} está falando.`);
+    },
+  };
+  
+  const comer = {
+    comer() {
+      console.log(`${this.nome} está comendo.`);
+    },
+  };
+  
+  const beber = {
+    beber() {
+      console.log(`${this.nome} está bebendo.`);
+    },
+  };
+  
+  const pessoaPrototype = Object.assign({}, falar, comer, beber);
+  
+  function criaPessoa(nome, sobrenome) {
+    return Object.create(pessoaPrototype, {
+      nome: { value: nome },
+      sobrenome: { value: sobrenome }
+    });
+  }
+  
+  const p1 = criaPessoa('Luiz', 'Otávio');
+  const p2 = criaPessoa('Maria', 'A.');
+  console.log(p2);
